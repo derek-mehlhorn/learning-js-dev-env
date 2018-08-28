5c00adb92fbd365935fac62bf07f3b47b34f73c0
Derek Mehlhorn <derekme@microsoft.com>
2018-08-28 14:29:00 -0700

    enabling linting using EsLint

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0.00');
-console.log(`Money: ${courseValue}`);
+console.log(`Money: ${courseValue}`); // eslint-disable-line no-console
